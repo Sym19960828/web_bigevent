89819d7283460519aacf2236342d88650d001a9e
itheima <itheima@qq.com>
2020-08-01 14:40:37 +0800

    首页功能完成

--- assets/js/baseApi.js
+++ assets/js/baseApi.js
@@ -4,5 +4,20 @@ var baseURL = 'http://ajax.frontend.itheima.net'
     //var baseURL="http://www.itcast.cn"
 $.ajaxPrefilter(function(options) {
     options.url = baseURL + options.url
+    if (options.url.indexOf('/my/') !== -1) {
+        options.headers = {
+            Authorization: localStorage.getItem("token") || " "
+        }
+    }
+    options.complete = function(res) {
+        var data = res.responseJSON;
+        console.log(data);
+        if (data.status == 1 && data.message == "身份认证失败！") {
+            //删除本地缓存token
+            localStorage.removeItem('token')
+                //退出到首页
+            location.href = "/login.html"
+        }
+    }
 
 });
